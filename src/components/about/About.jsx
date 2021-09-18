2b542a4e5545e168fb1d5fa30f842729bd106e4d
mansur3012 <mansur.rusnam94@gmail.com>
2021-09-18 23:46:07 +0700

    update about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,13 +4,24 @@ import "./About.scss";
 function About() {
   return (
     <div className="about" id="about">
-      <h1>About</h1>
-      <div>
-        <p>
-          I really like the world of technology, especially Frontend Development
-          and Software Development, and understand the team and be able to adapt
-          to future technological developments
-        </p>
+      <div className="line">
+        <h1>About</h1>
+        <div className="underline"></div>
+      </div>
+      <div className="container">
+        <div className="left">
+          <p>
+            I really like the world of technology, especially Frontend
+            Development and Software Development, and understand the team and be
+            able to adapt to future technological developments
+          </p>
+        </div>
+        <div className="right">
+          <img
+            src="https://images.unsplash.com/photo-1442328166075-47fe7153c128?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
